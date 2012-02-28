7b8303a2d489057bb6b1e0139f491fa5e62f8ce5
Siva K Reddybathula <siva.reddybathula@gmail.com>
2012-02-27 20:30:31 -0800

    added basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+
   protect_from_forgery
+  
+  protected
+  def authenticate
+      url = request.headers['PATH_INFO']
+      return true if url.end_with?('mobile') or url.end_with?"xml"
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'reddybathula' && password == 'SkreddyB'
+      end
+  end
 end
